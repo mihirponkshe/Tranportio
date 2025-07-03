815cf4e2641ff9dd4e18b3e8698178d259386d83
mihirponkshe <mihirponkshe2005@gmail.com>
2025-07-03 12:20:28 +0530

    Revert to previous stable version

--- sql_connection.py
+++ sql_connection.py
@@ -1,20 +1,13 @@
 import mysql.connector
-import os
 
 __cnx = None
 
 def get_sql_connection_cursor():
-    print("Opening MySQL connection...")
-    global __cnx
+  print("Opening mysql connection")
+  global __cnx
 
-    if __cnx is None:
-        __cnx = mysql.connector.connect(
-            host=os.getenv('DB_HOST'),
-            port=int(os.getenv('DB_PORT', 3306)),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            database=os.getenv('DB_NAME')
-        )
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='Mihir2701', database='transport')
 
-    return __cnx
+  return __cnx
 
